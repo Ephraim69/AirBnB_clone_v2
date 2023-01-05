f12fe25bf9d39e56effafd5fcf090ecd1b0bcb1b
Ephraim <ephraimigbinosa@gmail.com>
2023-01-05 21:46:28 +0100

    setup mysql dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,20 +1,8 @@
--- this script prepares a MySQL server for the project
--- create project developement database with the name : hbnb_dev_db
+-- prepares a MySQL server for the project
+-- creates a db, user and adds previledges to user
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- creating new user named : hbnb_dev with all privileges on the db hbnb_dev_db
--- with the password : hbnb_dev_pwd if it dosen't exist
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED
-BY 'hbnb_dev_pwd';
-
--- granting all privileges to the new user
-GRANT ALL PRIVELEGES
-ON hbnb_dev_db.*
-TO 'hbnb_dev'@'localhost';
-FLUSH PRIVELEGES;
-
--- granting the SELECT privilege for the user hbnb_dev in the db performance_schema
-GRANT SELECT
-ON performance_schema.*
-TO 'hbnb_dev'@'localhost';
-FLUSH PRIVELEGES;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
