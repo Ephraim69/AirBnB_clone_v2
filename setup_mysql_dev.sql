0ada67cbc27a897760ba7870933cb298d451e800
Ephraim <ephraimigbinosa@gmail.com>
2023-01-05 21:52:09 +0100

    setup mysql dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,16 +4,11 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- creating new user named : hbnb_dev with all privileges on the db hbnb_dev_db
 -- with the password : hbnb_dev_pwd if it dosen't exist
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED
-BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- granting all privileges to the new user
-GRANT ALL PRIVELEGES
-ON hbnb_dev_db.*
-TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVELEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- granting the SELECT privilege for the user hbnb_dev in the db performance_schema
-GRANT SELECT
-ON performance_schema.*
-TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVELEGES;
